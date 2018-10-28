16fbc6c263e378ceb43e099f287463d7121c3291
mk <kakazu.mariano@gmail.com>
2018-10-28 04:26:09 -0300

    Add example of unowned references

---
+++ referencias_sin_dueno.swift
@@ -0,0 +1,34 @@
+#!/usr/bin/env swift
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Card #\(number) is being deinitialized") }
+}
+
+// Define variables of type Custome?
+var john: Customer?
+
+// Create a new instance of Customer
+john = Customer(name: "John Appleseed")
+
+// Create a new instance of CreditCard
+// with a unowned reference to the instance of Customer
+john!.card = CreditCard(number: 1234_5678_9012_3456, customer: john!)
+
+john = nil
+// Prints "John Appleseed is being deinitialized"
+// Prints "Card #1234567890123456 is being deinitialized"
