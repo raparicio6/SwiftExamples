5a00a1afb974f4d5b3f1cc0da0c9b97f915cc868
mk <kakazu.mariano@gmail.com>
2018-10-29 16:20:26 -0300

    Add example of implicitly unwrapped optional property

---
+++ propiedad_opcional_implicitamente_desenvuelta.swift
@@ -0,0 +1,28 @@
+#!/usr/bin/env swift
+
+class Country {
+    let name: String
+    
+    // Implicitly Unwrapped Optional variable
+    var capitalCity: City!
+
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+// Define variable of type Country
+var country = Country(name: "Canada", capitalName: "Ottawa")
+
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+// Prints "Canada's capital city is called Ottawa"
