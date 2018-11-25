f3e9f7478121fb44f7ad0e6c5ebb3b8d8e741014
tcordeu <tcordeu@fi.uba.ar>
2018-11-25 16:27:26 -0300

    Optionals

---
+++ optionals.swift
@@ -0,0 +1,15 @@
+class Person {
+	var residence: Residence?
+}
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+//let roomCount = john.residence!.numberOfRooms
+//john.residence = Residence()
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
