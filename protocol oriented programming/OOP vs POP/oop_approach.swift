aea4b1048e2800b2c897dc09d218ecdb000f85ad
pintotomas <tomasp834@gmail.com>
2018-11-25 23:59:07 -0300

    complete code, some tests

--- protocol oriented programming/OOP vs POP/oop_approach.swift
+++ protocol oriented programming/OOP vs POP/oop_approach.swift
@@ -1,42 +1,61 @@
+import XCTest
+
 class Vehicle {
     var wheels: Int
     var isMoving: Bool
     var topSpeed: Int
-    var gasTank: Int
+    var filledQuantity: Int
     
     
-    init(wheels:Int = 4, isMoving:Bool = false, topSpeed:Int = 100, gasTank:Int = 100){
+    init(wheels:Int = 4, isMoving:Bool = false, topSpeed:Int = 100, filledQuantity:Int = 100){
         self.wheels = wheels
         self.isMoving = isMoving
         self.topSpeed = topSpeed
-        self.gasTank = gasTank
+        self.filledQuantity = filledQuantity
     }
     
-    func start(){}
-    func stop(){}
-    func park(){}
-    func fillGas(){}
+    func start(){
+    	self.isMoving = true
+    }
+    func stop(){
+    	self.isMoving = false
+    }
+    func fillGas(quantity:Int){
+    	self.filledQuantity += quantity
+    }
 }
 
 
 // Sports Vehicle class inherits from vehicle
-class SportsVehicle: Vehicle {
+class SuperVehicle: Vehicle {
     
-    var isTurbo: Bool
+    var turbo: Bool
     
-    func goTurbo(){}
-    
+    func goTurbo(){
+    	//Implementation here
+    }
+    func disableTurbo(){
+    	//Implementation here
+    }
+
     init(){
-        self.isTurbo = false
+        self.turbo = false
         super.init()
         
+    }
+
+    override func start(){
+    	self.turbo = true
+    	self.isMoving = true
     }
 }
 
 class ElectricVehicle: Vehicle {
     
     var batteryLevel: Int
-    func rechargeBattery(){}
+    func recharge(quantity: Int){
+    	self.batteryLevel += quantity
+    }
     
     init(){
         self.batteryLevel = 5
@@ -44,3 +63,37 @@ class ElectricVehicle: Vehicle {
     }
 }
 
+final class AppTests : XCTestCase {
+
+
+    func testGasCarRunningAfterStart(){
+    	let gasCar = Vehicle(wheels:4, isMoving:false,
+    						 topSpeed: 120, filledQuantity: 100)
+    	gasCar.start()
+    	XCTAssert(gasCar.isMoving)
+    }
+
+    func testSuperVehicleIsOnTurboAfterStart(){
+    	let superVehicle = SuperVehicle()
+    	superVehicle.start()
+    	XCTAssert(superVehicle.turbo)
+    }
+
+    func testElectricVehicleChargedAfterCharging(){
+    	let electricVehicle = ElectricVehicle()
+    	let batteryBeforeCharge = electricVehicle.batteryLevel
+    	electricVehicle.recharge(quantity: 50)
+    	let batteryAfterCharge = electricVehicle.batteryLevel
+    	XCTAssert(batteryAfterCharge > batteryBeforeCharge)
+    }
+
+    static let allTests = [
+        ("testGasCarRunningAfterStart", testGasCarRunningAfterStart),
+        ("testSuperVehicleIsOnTurboAfterStart", testSuperVehicleIsOnTurboAfterStart),
+        ("testElectricVehicleChargedAfterCharging", testElectricVehicleChargedAfterCharging)
+    ]
+}
+
+XCTMain([
+    testCase(AppTests.allTests),
+])
--- protocol oriented programming/OOP vs POP/pop_approach.swift
+++ protocol oriented programming/OOP vs POP/pop_approach.swift
@@ -1,32 +1,125 @@
-// Electric Cars
-protocol Chargeable {
-    func recharge()
-}
+import XCTest
 
 // Gas Cars
 protocol Fillable {
-    func fillTank()
+    var filledQuantity: Int { get set } 
+    mutating func fillTank(quantity: Int)
+}
+
+extension Fillable {
+	mutating func fillTank(quantity: Int){
+		self.filledQuantity += quantity
+	}
 }
 
 // Features Needed by both
 protocol Driveable {
-    func start()
-    func stop()
-    func park()
+    var wheels: Int { get }
+    var isMoving: Bool { get set }
+    var topSpeed: Int { get }
+    mutating func start()
+    mutating func stop()
 }
 
-// Now each struct only uses the features it needs!
-struct ElectricVehicle: Driveable, Chargeable{
-    
+extension Driveable {
+
+	mutating func start(){
+		self.isMoving = true
+	}
+
+	mutating func stop(){
+		self.isMoving = false
+	}
+
 }
 
-struct SportsVehicle: Driveable, Fillable{
-    
+struct GasCar: Fillable, Driveable {
+	var filledQuantity: Int 
+	var wheels: Int 
+    var isMoving: Bool 
+    var topSpeed: Int 
+}
+
+
+struct SuperVehicle: Fillable, Driveable {
+
+	var filledQuantity: Int 
+	var wheels: Int 
+    var isMoving: Bool 
+    var topSpeed: Int 
+    var turbo: Bool
+
+    mutating func start(){
+    	self.isMoving = true
+    	self.turbo = true
+    }
+}
+
+
+//We now requiere electric cars
+
+// Electric Cars
+protocol Chargeable {
+    var batteryLevel: Int { get set } 
+    mutating func recharge(quantity: Int)
 }
 
 // extending for classes
-extension Chargeable where Self:ElectricVehicle{
-    func recharge(batteryLevel:Int){
-        self.batteryLevel = 100
+extension Chargeable{
+    mutating func recharge(quantity: Int){
+        self.batteryLevel += quantity
     }
 }
+
+
+// Now each struct only uses the features it needs!
+struct ElectricVehicle: Driveable, Chargeable{
+    var wheels: Int 
+    var isMoving: Bool 
+    var topSpeed: Int 
+    var batteryLevel: Int
+}
+
+/*Unrelated objects*/
+
+struct Bucket: Fillable {
+	var filledQuantity: Int
+}
+
+
+final class AppTests : XCTestCase {
+
+
+    func testGasCarRunningAfterStart(){
+    	var gasCar = GasCar(filledQuantity: 100, wheels:4,
+    						 isMoving:false, topSpeed: 120)
+    	gasCar.start()
+    	XCTAssert(gasCar.isMoving)
+    }
+
+    func testSuperVehicleIsOnTurboAfterStart(){
+    	var superVehicle = SuperVehicle(filledQuantity: 100, wheels:4,
+    									 isMoving:false, topSpeed: 120, turbo:false)
+    	superVehicle.start()
+    	XCTAssert(superVehicle.turbo)
+    }
+
+    func testElectricVehicleChargedAfterCharging(){
+    	var electricVehicle = ElectricVehicle(wheels: 4, isMoving: false,
+    						 topSpeed: 120, batteryLevel: 50)
+    	let batteryBeforeCharge = electricVehicle.batteryLevel
+    	electricVehicle.recharge(quantity: 50)
+    	let batteryAfterCharge = electricVehicle.batteryLevel
+    	XCTAssert(batteryAfterCharge > batteryBeforeCharge)
+    }
+
+    static let allTests = [
+        ("testGasCarRunningAfterStart", testGasCarRunningAfterStart),
+        ("testSuperVehicleIsOnTurboAfterStart", testSuperVehicleIsOnTurboAfterStart),
+        ("testElectricVehicleChargedAfterCharging", testElectricVehicleChargedAfterCharging)
+    ]
+}
+
+XCTMain([
+    testCase(AppTests.allTests),
+])
