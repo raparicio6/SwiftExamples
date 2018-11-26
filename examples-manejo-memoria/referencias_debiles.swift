56a2f08d068971bd37ce81ed17aacf82c843d607
mk <kakazu.mariano@gmail.com>
2018-11-26 13:14:31 -0300

    Clean up code

--- examples-manejo-memoria/referencias_debiles.swift
+++ examples-manejo-memoria/referencias_debiles.swift
@@ -1,27 +1,29 @@
 class Person {
     let name: String
-    init(name: String) { self.name = name }
     var apartment: Apartment?
-    deinit { print("\(name) is being deinitialized") }
+
+    init(name: String) { self.name = name 
+    }
+    deinit { print("\(name) is being deinitialized") 
+    }
 }
 
 class Apartment {
     let unit: String
-    init(unit: String) { self.unit = unit }
     weak var tenant: Person?
-    deinit { print("Apartment \(unit) is being deinitialized") }
+
+    init(unit: String) { 
+        self.unit = unit 
+    }
+    deinit { 
+        print("Apartment \(unit) is being deinitialized") 
+    }
 }
 
-// Define variables of type Person?
 var john: Person?
-
-// Define variables of type Apartment?
 var unit4A: Apartment?
 
-// Create a new instance of Person
 john = Person(name: "John Appleseed")
-
-// Create a new instance of Apartment
 unit4A = Apartment(unit: "4A")
 
 // Link the two instances together, 
--- examples-manejo-memoria/referencias_sin_dueno.swift
+++ examples-manejo-memoria/referencias_sin_dueno.swift
@@ -1,26 +1,29 @@
 class Customer {
     let name: String
     var card: CreditCard?
+
     init(name: String) {
         self.name = name
     }
-    deinit { print("\(name) is being deinitialized") }
+    deinit { 
+        print("\(name) is being deinitialized") 
+    }
 }
 
 class CreditCard {
     let number: UInt64
     unowned let customer: Customer
+
     init(number: UInt64, customer: Customer) {
         self.number = number
         self.customer = customer
     }
-    deinit { print("Card #\(number) is being deinitialized") }
+    deinit { 
+        print("Card #\(number) is being deinitialized") 
+    }
 }
 
-// Define variables of type Customer?
 var john: Customer?
-
-// Create a new instance of Customer
 john = Customer(name: "John Appleseed")
 
 // Create a new instance of CreditCard
