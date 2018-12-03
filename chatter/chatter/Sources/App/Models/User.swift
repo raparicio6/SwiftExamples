ddfe861a427122a73fbb3cb243f9e0ee6ae0a00c
mk <kakazu.mariano@gmail.com>
2018-12-02 22:58:23 -0300

    Minimal

--- chatter/chatter/Sources/App/Models/User.swift
+++ chatter/chatter/Sources/App/Models/User.swift
@@ -5,17 +5,10 @@ import Vapor
 final class User: Content, Parameter {
     var id: UUID?
     var username: String
-    //var firstname: String
-    //var lastname: String
-    //var email: String
     var password: String
     
-    //init(username: String, firstname: String, lastname: String, email: String, password: String) {
     init(username: String, password: String) {
         self.username = username
-        //self.firstname = firstname
-        //self.lastname = lastname
-        //self.email = email
         self.password = password
     }
 }
@@ -31,7 +24,6 @@ extension User: Migration {
         return Database.create(self, on: connection) { builder in
             try addProperties(to: builder)
             builder.unique(on: \.username)
-            //builder.unique(on: \.email)
         }
     }
 }
